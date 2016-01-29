1d5c3ec1a870b55d3e446f075b493641a6a50c5f
Michael H. Arieli <michaelharieli@gmail.com>
2016-01-29 11:51:17 -0800

    Make JSON string pretty

--- src/Generator/JsonGenerator.php
+++ src/Generator/JsonGenerator.php
@@ -48,4 +48,84 @@ class JsonGenerator extends Generator
     {
         echo sprintf("<pre>Filename = %s\n\n%s</pre><hr />", $target, $this->render($template, $parameters));
     }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function render($template, $parameters)
+    {
+        return $this->prettyPrint(parent::render($template, $parameters));
+    }
+
+    /**
+     * Make a JSON string look good :-).
+     *
+     * @param string $json
+     *
+     * @return string
+     */
+    public static function prettyPrint($json)
+    {
+        $result = '';
+        $level = 0;
+        $prevChar = '';
+        $inQuotes = false;
+        $endsLineLevel = null;
+
+        for ($i = 0; $i < strlen($json); $i++) {
+            $char = $json[$i];
+            $newLineLevel = null;
+            $post = '';
+
+            if ($endsLineLevel !== null) {
+                $newLineLevel = $endsLineLevel;
+                $endsLineLevel = null;
+            }
+
+            if ($char === '"' && $prevChar != '\\') {
+                $inQuotes = !$inQuotes;
+            } elseif (!$inQuotes) {
+                switch ($char) {
+                    case '}':
+                    case ']':
+                        $level--;
+                        $endsLineLevel = null;
+                        $newLineLevel = $level;
+                        break;
+
+                    case '{':
+                    case '[':
+                        $level++;
+                        $endsLineLevel = $level;
+                        break;
+
+                    case ',':
+                        $endsLineLevel = $level;
+                        break;
+
+                    case ':':
+                        $post = ' ';
+                        break;
+
+                    case " ":
+                    case "\t":
+                    case "\n":
+                    case "\r":
+                        $char = "";
+                        $endsLineLevel = $newLineLevel;
+                        $newLineLevel = null;
+                        break;
+                }
+            }
+
+            if ($newLineLevel !== null) {
+                $result .= "\n".str_repeat("  ", $newLineLevel);
+            }
+
+            $result .= $char.$post;
+            $prevChar = $char;
+        }
+
+        return $result;
+    }
 }
