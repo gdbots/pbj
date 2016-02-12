61293098d15baa6c52008b2f1f3a8ac54c54a7a9
Michael H. Arieli <michaelharieli@gmail.com>
2016-02-12 15:05:22 -0800

    Added validator todo's

--- src/SchemaValidator.php
+++ src/SchemaValidator.php
@@ -29,11 +29,19 @@ class SchemaValidator
             ->add(new Assert\IsMixinSchemeTypeConstraint())
             ->add(new Assert\RemoveSchemeMixinConstraint())
             ->add(new Assert\RemoveSchemeEnumConstraint())
-            ->add(new Assert\RemoveSchemeFieldConstraint())
+            ->add(new Assert\RemoveSchemeFieldConstraint()) //todo: check inherit field from attached mixins
             ->add(new Assert\EnumTypeConstraint())
             ->add(new Assert\EnumOptionConstraint())
 
             //todo: additional constraints
+            //->add(new Assert\RequireAddtionalFieldConstraint())
+            //->add(new Assert\FieldRestrictAttributeConstraint())
+            //->add(new Assert\FieldPatternConstraint())
+            //->add(new Assert\FieldDefaultConstraint())
+            //->add(new Assert\FieldEnumConstraint())
+            //->add(new Assert\FieldAnyOfConstraint())
+
+            // maybe add one for each field type?
 
             ->validate($prevSchema, $schema)
         ;
