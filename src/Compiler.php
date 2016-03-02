2bd5d8feed836f081868290b6841d8330c66ba1c
Michael H. Arieli <michaelharieli@gmail.com>
2016-03-02 09:31:31 -0800

    Fixed typo - throw exception if preg_grep return false

--- src/Compiler.php
+++ src/Compiler.php
@@ -69,7 +69,7 @@ final class Compiler
             } catch (MissingSchema $e) {
                 $files = preg_grep(sprintf('/%s*/', str_replace([':v', ':'], [':', '\/'], $e->getMessage())), $schemas);
 
-                if (count($files) === 0) {
+                if ($files === false || count($files) === 0) {
                     throw new \RuntimeException(sprintf('Schema with id "%s" is invalid.', $e->getMessage()));
                 }
 
