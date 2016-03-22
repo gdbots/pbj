ec702158b4fb5fd40d17141e3193c34f255a1049
Michael H. Arieli <michaelharieli@gmail.com>
2016-03-22 14:33:49 -0700

    Renamed simple to compiler and move to web folder

--- examples/web/compiler.php
+++ examples/web/compiler.php
@@ -4,8 +4,8 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require __DIR__.'/../vendor/autoload.php';
-require __DIR__.'/pbj-schema-stores.php';
+require __DIR__.'/../../vendor/autoload.php';
+require __DIR__.'/../pbj-schema-stores.php';
 
 use Gdbots\Pbjc\Compiler;
 use Gdbots\Pbjc\CompileOptions;
@@ -15,11 +15,13 @@ $compiler = new Compiler();
 
 $namespaces = ['acme:blog', 'acme:core'];
 
+$rootDir = __DIR__.'/../';
+
 // generate PHP files
 $compiler->run('php', new CompileOptions([
     'namespaces' => $namespaces,
-    'output' => __DIR__.'/src',
-    'manifest' => __DIR__.'/pbj-schemas.php',
+    'output' => $rootDir.'/src',
+    'manifest' => $rootDir.'/pbj-schemas.php',
     'callback' => function (OutputFile $file) {
         echo highlight_string($file->getContents(), true).'<hr />';
 
@@ -33,9 +35,9 @@ $compiler->run('php', new CompileOptions([
 
 // generate JSON-SCHEMA files
 $compiler->run('json-schema', new CompileOptions([
-    'domain' => 'http://example.com/schemas',
+    'domain' => 'http://pbjc.local/json-schema',
     'namespaces' => $namespaces,
-    'output' => __DIR__.'/json-schema',
+    'output' => $rootDir.'/json-schema',
     'callback' => function (OutputFile $file) {
         echo highlight_string($file->getContents(), true).'<hr />';
 
