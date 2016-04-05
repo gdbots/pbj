2e2e24d87746c9f6434917e898554f24d878df24
Michael H. Arieli <michaelharieli@gmail.com>
2016-04-05 14:20:52 -0700

    RE #13: Added more json-schema test examples

--- tests/JsonValidatorTest.php
+++ tests/JsonValidatorTest.php
@@ -9,12 +9,18 @@ use JsonSchema\Validator;
 
 class JsonValidatorTest extends \PHPUnit_Framework_TestCase
 {
-    public function testValidateSchema()
+    /**
+     * @dataProvider getSchemas
+     */
+    public function testValidateSchema($data)
     {
+        if (is_string($data)) {
+            $data = json_decode(file_get_contents($data));
+        }
+
         // get the schema and data as objects
         $retriever = new UriRetriever();
         $schema = $retriever->retrieve('http://json-schema.org/draft-04/schema#');
-        $data = json_decode(file_get_contents(__DIR__.'/Fixtures/json-schema/article.json'));
 
         // resolve $ref's
         $refResolver = new RefResolver($retriever);
@@ -25,5 +31,55 @@ class JsonValidatorTest extends \PHPUnit_Framework_TestCase
         $validator->check($data, $schema);
 
         $this->assertTrue($validator->isValid());
+    }
+
+    /**
+     * @return array
+     */
+    public function getSchemas()
+    {
+        return [
+            [
+                'data' => __DIR__.'/Fixtures/json-schema/article.json'
+            ],
+            [
+                'data' => __DIR__.'/Fixtures/json-schema/comment.json'
+            ],
+            [
+                'data' => (object)[
+                    'id' => 'file://schame.json#',
+                    '$schema' => 'http://json-schema.org/draft-04/schema#',
+                    'type' => 'object',
+                    'properties' => (object)[
+                        '_schema' => (object)[
+                            'type' => 'string',
+                            'pattern' => '^pbj:([a-z0-9-]+):([a-z0-9\\.-]+):([a-z0-9-]+)?:([a-z0-9-]+):([0-9]+-[0-9]+-[0-9]+)$',
+                            'default' => 'pbj:acme:blog:entity:manual-schema:1-0-0'
+                        ]
+                    ],
+                    'additionalProperties' => false
+                ]
+            ],
+            [
+                'data' => (object)[
+                    'id' => 'file://schame.json#',
+                    '$schema' => 'http://json-schema.org/draft-04/schema#',
+                    'type' => 'object',
+                    'properties' => (object)[
+                        'tags' => (object)[
+                            'type' => 'array',
+                            'items' => (object)[
+                                (object)[
+                                    'type' => 'string',
+                                ]
+                            ],
+                            'minItems' => 1,
+                            'uniqueItems' => true
+                        ]
+                    ],
+                    'additionalProperties' => false
+                ]
+            ]
+        ];
     }
 }
