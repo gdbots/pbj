9a79d85ac49f78d655b2cc43c36e2b994176adc7
Michael H. Arieli <michaelharieli@gmail.com>
2017-03-15 13:24:47 -0700

    RE #40: Fixed schema inheritance field check

--- src/Validator/SchemaInheritanceFields.php
+++ src/Validator/SchemaInheritanceFields.php
@@ -2,6 +2,7 @@
 
 namespace Gdbots\Pbjc\Validator;
 
+use Gdbots\Common\Util\StringUtils;
 use Gdbots\Pbjc\SchemaDescriptor;
 use Gdbots\Pbjc\FieldDescriptor;
 
@@ -45,15 +46,112 @@ class SchemaInheritanceFields implements Constraint
                     $fa = $currentFields[$name];
                     $fb = $inheritedFields[$name];
 
-                    if ($fa && $fb && $fa->$method() != $fb->$method()) {
-                        throw new \RuntimeException(sprintf(
-                            'The schema "%s" field "%s" is invalid. See inherited mixin fields.',
-                            $a->getId()->toString(),
-                            $property->getName()
-                        ));
+                    if ($fa && $fb) {
+                        switch ($method) {
+                            case 'getAnyOf':
+                                if (!$fa->$method() && !$fb->$method()) {
+                                    continue 2;
+                                }
+
+                                $ea = [];
+                                foreach ((array) $fa->$method() as $schema) {
+                                    $ea[] = $this->getClassName($schema);
+
+                                    if ($extends = $schema->getExtends()) {
+                                        do {
+                                            $ea[] = $this->getClassName($extends);
+                                        } while ($extends = $extends->getExtends());
+                                    }
+                                }
+
+                                $eb = [];
+                                foreach ((array) $fb->$method() as $schema) {
+                                    $eb[] = $this->getClassName($schema);
+
+                                    if ($extends = $schema->getExtends()) {
+                                        do {
+                                            $eb[] = $this->getClassName($extends);
+                                        } while ($extends = $extends->getExtends());
+                                    }
+                                }
+
+                                $ea = array_reverse($ea);
+                                $eb = array_reverse($eb);
+
+                                $phpClasses = [];
+                                foreach ([$ea, $eb] as $classes) {
+                                    foreach ($classes as $index => $class) {
+                                        if (class_exists($class)) {
+                                            continue;
+                                        }
+
+                                        if ($index === 0) {
+                                            $class = sprintf('class %s {};', $class);
+                                        } else {
+                                            $class = sprintf('class %s extends %s {};', $class, $classes[$index-1]);
+                                        }
+
+                                        if (!in_array($class, $phpClasses)) {
+                                            $phpClasses[] = $class;
+                                        }
+                                    }
+                                }
+
+                                if (count($phpClasses)) {
+                                    eval(implode("\n", $phpClasses));
+                                }
+
+                                // missing exnteded anyOf class
+                                if (!isset($ea[0]) || !isset($eb[0])) {
+                                    throw new \RuntimeException(sprintf(
+                                        'The schema "%s" field "%s" is invalid. See inherited mixin fields.',
+                                        $a->getId()->toString(),
+                                        $property->getName()
+                                    ));
+                                }
+
+                                $oa = new $ea[0]();
+                                $ob = new $eb[0]();
+
+                                // not class is not inherited
+                                if (!$oa instanceof $ob) {
+                                    throw new \RuntimeException(sprintf(
+                                        'The schema "%s" field "%s" is invalid. See inherited mixin fields.',
+                                        $a->getId()->toString(),
+                                        $property->getName()
+                                    ));
+                                }
+
+                                break;
+
+                            default:
+                                if ($fa->$method() != $fb->$method()) {
+                                    throw new \RuntimeException(sprintf(
+                                        'The schema "%s" field "%s" is invalid. See inherited mixin fields.',
+                                        $a->getId()->toString(),
+                                        $property->getName()
+                                    ));
+                                }
+                        }
                     }
                 }
             }
         }
+    }
+
+    /**
+     * @param SchemaDescriptor $schema
+     *
+     * @return string
+     */
+    private function getClassName(SchemaDescriptor $schema)
+    {
+        return sprintf(
+            '%s%s%sV%s',
+            StringUtils::toCamelFromSlug($schema->getId()->getVendor()),
+            StringUtils::toCamelFromSlug($schema->getId()->getPackage()),
+            StringUtils::toCamelFromSlug($schema->getId()->getMessage()),
+            preg_replace('/-/', '', $schema->getId()->getVersion())
+        );
     }
 }
